35535c869266c61e24015734f9287d47fd6c1f7c
Joe Anderson <yoej262@gmail.com>
2016-08-08 04:34:03 +0000

    Working version

--- server.js
+++ server.js
@@ -1,29 +1,14 @@
 var express = require('express');
 
 var app = express();
-var months= ["January","Febuary","March","April","May","June","July","August","September","October","November","December"];
-app.get('/:DATE', function (req, res) {
-    var val = req.params.DATE;
-    var testExp = /^[0-9]{10}/;
-    var date;
-    var testDate = Date.parse(val).toString();
-    var returnDate = {"unix": null,"natural":null};
-    var setReturn = function(setDate){
-      returnDate.unix = (Date.parse(setDate) / 1000).toString();
-      returnDate.natural = months[setDate.getMonth()]+ " " + setDate.getDate() + ", " + setDate.getFullYear();
-    };
-    if (testExp.test(val.toString()) === true) {
-      date = new Date(val * 1000);
-      setReturn(date);
-      res.send(returnDate);
-    } else if (testExp.test(testDate) === true) {
-      date = new Date(val);
-      setReturn(date);
-      res.send(returnDate);
-    } else {
-      res.send(returnDate);
-    }
-    
+var getIP = require('ipware')().get_ip;
+var useragent= require("useragent");
+app.get('/', function (req, res) {
+    var agent = useragent.parse(req.headers['user-agent']).os.toString();
+    var ipInfo = getIP(req).clientIp;
+    var language = req.headers["accept-language"].split(",")[0];
+    var returnData = {"ipaddress" : ipInfo, "language" : language, "software" : agent};
+    res.send(returnData);
 });
 
 app.listen(8080, function () {
